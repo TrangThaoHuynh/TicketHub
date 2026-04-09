da5d1425522029fc8e32bfd813e541248c027db9
Thảo Trang <139950702+TrangThaoHuynh@users.noreply.github.com>
2026-04-09 12:52:05 +0700

    save stash before switch branch

--- app/services/ticket_service.py
+++ app/services/ticket_service.py
@@ -1,7 +1,29 @@
 from ..models.ticket import Ticket
+from ..models.ticket_type import TicketType 
 from .. import db
 import uuid
+from sqlalchemy import func
 from datetime import datetime
+
+#Lay danh sach loai ve cua 1 su kien
+def get_ticket_types_by_event_id(event_id):
+    return TicketType.query.filter_by(eventId=event_id).all()
+
+#dem so luong ve da bán
+def count_sold_by_ticket_type(ticket_type_ids: list[int])-> dict[int, int]:
+    if not ticket_type_ids:
+        return {}
+
+    rows = (
+    db.session.query(Ticket.ticketTypeId, func.count(Ticket.id)) 
+    .filter(
+        Ticket.ticketTypeId.in_(ticket_type_ids), 
+        Ticket.status.in_(["ACTIVE", "USED"])
+    )
+    .group_by(Ticket.ticketTypeId)
+    .all()
+)
+    return {tid: cnt for tid, cnt in rows}
 
 def create_ticket(data):
     ticket = Ticket(
