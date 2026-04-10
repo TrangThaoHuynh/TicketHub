6da8cea874f70dced0054c163de1eb2d3acdfbf2
DaoNgocQuy <139777419+DaoNgocQuy@users.noreply.github.com>
2026-04-10 16:01:28 +0700

    ET-54

--- app/routes/main.py
+++ app/routes/main.py
@@ -7,6 +7,19 @@ main = Blueprint('main', __name__)
 def index():
     keyword = request.args.get("keyword")
     event_type_id = request.args.get("eventTypeId")
+    start_date = request.args.get("startDate")
+    end_date = request.args.get("endDate")
+    location = request.args.get("location")
+    price_min = request.args.get("priceMin")
+    price_max = request.args.get("priceMax")
     event_types = get_event_types()
-    events = get_home_events(keyword=keyword, event_type_id=event_type_id)
-    return render_template("main.html", event_types=event_types, events=events)
+    events = get_home_events(
+        keyword=keyword,
+        event_type_id=event_type_id,
+        start_date=start_date,
+        end_date=end_date,
+        location=location,
+        price_min=price_min,
+        price_max=price_max,
+    )
+    return render_template("main.html", event_types=event_types, events=events, show_search=True)
